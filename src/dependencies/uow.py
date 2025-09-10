c4877583bca08808c360a555a55b6705659a5d3f
alufirenko <alufirenko@quatromatic.com>
2025-09-10 12:07:57 +0700

    add dep uow

---
+++ src/dependencies/uow.py
@@ -0,0 +1,10 @@
+from src.uow.sqlalchemy import UnitOfWork
+from src.database.connection import async_session_maker
+
+
+async def get_uow():
+    async with UnitOfWork(async_session_maker) as uow:
+        yield uow
+
+
+
